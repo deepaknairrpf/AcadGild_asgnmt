fa9077a1597a5f9bcf73f021c8945e0d7e26b112
deepaknairrpf <cse15b016@iiitk.ac.in>
2016-05-23 12:21:48 +0530

    Add files via upload

---
+++ Assignment_3.java
@@ -0,0 +1,13 @@
+
+public class Assignment_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                int number1=3.2;
+                int number2=232342355432232324343;
+	}
+
+}
